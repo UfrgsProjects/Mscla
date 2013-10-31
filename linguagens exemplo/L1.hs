7b35b59016ddf81973d44fbc427cd0690e3e6a12
Felipe2 <felipe2@felipe-System-Product-Name.(none)>
2013-10-31 20:38:19 -0200

    starting 2

--- linguagens exemplo/L1.hs
+++ linguagens exemplo/L1.hs
@@ -1,12 +1,26 @@
 
+import Data.IORef
+import Text.ParserCombinators.Parsec
+import Graphics.UI.Gtk
 
--- Na verdade é um tipo composto, variaveis sao um nome mais um tipo
-type Var = String
+-- Uma variavel, é um tipo assoc a um nome
+data MsclaVar = Var MsclaType String
 
+instance Show MsclaVar where
+  show (Var aType aName) = (show aType)++":"++aName
+
+-- Tipos do sistema de tipos
+data MsclaType = MsclaInt
+               | MsclaBool
+
+instance Show MsclaType where
+  show (MsclaInt) = "int"
+  show (MsclaBool) = "bool"
 
 data Bool = True
           | False
 
+-- Conjunto de operacoes
 data Op = MsclaPlus 
         | MsclaMinus
         | MsclaLeq
@@ -16,12 +30,14 @@ instance Show Op where
   show MsclaMinus = "-"
   
 
+-- Gramatica de termos
 data Term = Number Integer
-          | Fun Var Term
+          | Fun MsclaVar Term
           | If Term Term Term
           | Bool 
           | MsclaOp Op Term Term
-          | Let Var Term
+          | Let MsclaVar Term
+          | App Term Term
          
             
 instance Show Term where
@@ -30,11 +46,21 @@ instance Show Term where
   show (Fun x y) = "fn "++(show x)++" => "++(show y)
   show (MsclaOp op term1 term2) = (show term1)++" "++(show op)++" "++(show term2)
   
+
+-- *** FUNCOES DE TESTE DOS TIPOS CRIADOS *** --
 makeMsclaInt :: Integer -> Term
 makeMsclaInt x =  Number x
 
-makeMsclaFun :: String->Term->Term
-makeMsclaFun x y = Fun x y
+makeMsclaType :: String -> MsclaType
+makeMsclaType x = case x of
+  "int" -> MsclaInt
+  _ -> MsclaBool
+
+makeMsclaVar :: String->String->MsclaVar
+makeMsclaVar x y = Var (makeMsclaType x) y
+
+makeMsclaFun :: String->String->Term->Term
+makeMsclaFun x y z = Fun (makeMsclaVar x y) z
 
 makeSum :: Integer -> Term
 makeSum x = MsclaOp MsclaPlus (makeMsclaInt x) (makeMsclaInt x)

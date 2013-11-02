0c3fd79d76b90dcf38d08b3e8d4f1f0cfe507f08
Felipe2 <felipe2@felipe-System-Product-Name.(none)>
2013-11-02 18:56:37 -0200

    probs em operacoes

--- linguagens exemplo/L1.hs
+++ linguagens exemplo/L1.hs
@@ -1,13 +1,15 @@
 
+import System.Environment
 import Data.IORef
 import Text.ParserCombinators.Parsec
 import Graphics.UI.Gtk
+import Control.Monad
 
 -- Uma variavel, é um tipo assoc a um nome
-data MsclaVar = Var MsclaType String
+data MsclaVar = Var String MsclaType
 
 instance Show MsclaVar where
-  show (Var aType aName) = (show aType)++":"++aName
+  show (Var aName aType) = aName++":"++(show aType)
 
 -- Tipos do sistema de tipos
 data MsclaType = MsclaInt
@@ -34,17 +36,21 @@ instance Show Op where
 data Term = Number Integer
           | Fun MsclaVar Term
           | If Term Term Term
-          | Bool 
+          | T 
+          | F
           | MsclaOp Op Term Term
           | Let MsclaVar Term
           | App Term Term
          
             
 instance Show Term where
-  show (Number x) = (show x)
+  show (Number x) = (show x)++"tchau"
   show (If x y z) = "(if "++(show x)++" then "++(show y)++" else "++(show z)++")"
   show (Fun x y) = "fn "++(show x)++" => "++(show y)
-  show (MsclaOp op term1 term2) = (show term1)++" "++(show op)++" "++(show term2)
+  show (MsclaOp op term1 term2) = ((show term1)++" "++(show op)++" "++(show term2)++"oi")
+  show (Let aVar aTerm) = "let "++(show aVar)++" = "++(show aTerm)
+  show T = "true"
+  show F = "false"
   
 
 -- *** FUNCOES DE TESTE DOS TIPOS CRIADOS *** --
@@ -57,7 +63,7 @@ makeMsclaType x = case x of
   _ -> MsclaBool
 
 makeMsclaVar :: String->String->MsclaVar
-makeMsclaVar x y = Var (makeMsclaType x) y
+makeMsclaVar x y = Var y (makeMsclaType x) 
 
 makeMsclaFun :: String->String->Term->Term
 makeMsclaFun x y z = Fun (makeMsclaVar x y) z
@@ -65,3 +71,120 @@ makeMsclaFun x y z = Fun (makeMsclaVar x y) z
 makeSum :: Integer -> Term
 makeSum x = MsclaOp MsclaPlus (makeMsclaInt x) (makeMsclaInt x)
 
+
+
+
+parseNumber :: Parser Term
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseVar :: Parser MsclaVar
+parseVar = do  theName <- many (letter <|> digit)
+               string ":"
+               theType <- many (letter <|> digit)
+               return $ Var theName MsclaInt
+               
+
+symbol :: Parser Char
+symbol = oneOf "!$%&|*+-/:<=>?@^_~"
+
+parseOp :: Parser Op
+parseOp = do theSymbol <- many (symbol)
+             return $ case theSymbol of 
+                        "+" -> MsclaPlus
+                        "-" -> MsclaMinus
+                        
+              
+parseExpr :: Parser Term
+parseExpr = (do string "if"
+                spaces
+                t1 <- parseExpr
+                spaces
+                string "then"
+                spaces
+                t2 <- parseExpr
+                spaces
+                string "else"
+                spaces
+                t3 <- parseExpr
+                spaces
+                return $ If t1 t2 t3)
+            <|>
+            (do string "true"
+                spaces
+                return $ T)
+            <|>
+            (do string "false"
+                spaces
+                return $ F)
+            <|>
+            (do string "let"
+                spaces
+                t1 <- parseVar
+                spaces
+                string "="
+                spaces
+                t2 <- parseExpr
+                spaces
+                return $ Let t1 t2)
+            <|>
+            (do string "xn" --nao posso colocar nenhuma letra tipo f ou l
+                spaces
+                t1 <- parseVar
+                spaces
+                string "=>"
+                spaces
+                t2 <- parseExpr
+                spaces
+                return $ Fun t1 t2)
+            <|> 
+            (do string "("    
+                spaces
+                t <- parseExpr
+                spaces
+                string ")"
+                spaces
+                return $ t)
+            <|>
+            (do t1 <- parseExpr
+                spaces
+                op <- parseOp
+                spaces
+                t2 <- parseExpr
+                spaces
+                return $ MsclaOp op t1 t2)
+            <|>
+            	parseNumber
+            
+            
+              
+readExpr :: String -> String
+readExpr input = case parse parseExpr "mscla" input of
+    Left err -> "No match: " ++ show err
+    Right t -> show t
+
+readOp :: String -> String
+readOp input = case parse parseOp "msclaOP" input of
+    Left err -> "No match: " ++ show err
+    Right t -> show t
+    
+{-
+main :: IO ()
+main = do args <- getArgs
+          putStrLn (readExpr (args !! 0))
+-}
+
+
+main :: IO ()
+main = putStrLn $ show $ readExpr ("1 + 1")
+
+
+
+{-
+main :: IO ()
+main = putStrLn $ show $ readExpr "xn x:int => (( if true then (if true then true else false) else true ) + 1)"
+-}
+
+{-
+main :: IO ()
+main = putStrLn $ show $ readOp "1 + 1"
+-}
